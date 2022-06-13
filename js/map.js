89f1205fd3ceedfe81cb13ae9b68220436b71998
Suman <tdkasman2@gmail.com>
2022-06-13 23:04:04 +0545

    small bug fixed

--- js/map.js
+++ js/map.js
@@ -619,7 +619,7 @@ function updateTable() {
         } else {
           document.getElementById("networkName").innerHTML = "ALL NETWORKS";
         }
-        document.getElementById("numNodes").innerHTML = ISPSum;
+        document.getElementById("numNodes").innerHTML =":"+ ISPSum;
         if ($.fn.dataTable.isDataTable("#dataTable")) {
           //
           $("#dataTable").DataTable().destroy();
@@ -704,7 +704,7 @@ function updateTable() {
         } else {
           document.getElementById("networkName").innerHTML = "ALL NETWORKS";
         }
-        document.getElementById("numNodes").innerHTML = DCSSum;
+        document.getElementById("numNodes").innerHTML =":"+ DCSSum;
         if ($.fn.dataTable.isDataTable("#dataTable")) {
           $("#dataTable").DataTable().destroy();
         }
